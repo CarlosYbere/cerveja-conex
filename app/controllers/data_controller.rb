876133d6fbcfa2821852da1affc9ed08e8e68d11
Bruno Vieira Costa <brunopsycon@gmail.com>
2015-12-09 23:23:44 -0200

    entrada e saida via internet

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,12 @@
 class DataController < ApplicationController
 	def light
-		#aqui eu vejo a luz
+		#aqui eu vejo a luz'
+		@lightstatus = true
+		@lightonoff = true #aqui eu coloc
+		respond_to do |format|
+			format.js
+			format.html
+		end
 	end
 	def sensors
 		#aqui eu me comunico com os sensores...
@@ -8,9 +14,9 @@ class DataController < ApplicationController
 		@temperature = 40
 	end
 	def multimedia
-
+		@link = "http.xxx.x.xx.x.xxx"
 	end
 	def plot
-
+		@valor = 5
 	end
 end
