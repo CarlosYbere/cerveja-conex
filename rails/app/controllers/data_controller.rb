031ff1ebf55efbb521b6387afd25e40cf58f0ae6
LFlopes <lfcl@poli.ufrj.br>
2015-12-11 22:04:45 -0200

    colocamos a leitura serial para funcionar

--- rails/app/controllers/data_controller.rb
+++ rails/app/controllers/data_controller.rb
@@ -1,9 +1,45 @@
 class DataController < ApplicationController
+	require 'rubyserial'
+	require 'nokogiri'
 	def light
+
+		port_str = `ls /dev`.split("\n").grep(/usb|ACM/i).map{|d| "/dev/#{d}"}[0] #may be different for you
+		baud_rate = 115200
+		serialport = Serial.new port_str, baud_rate
+
 		#aqui eu vejo a luz'
 		@lightstatus = true
 		@lightonoff = true #aqui eu coloc
+
+		leitura = serialport.read 300
+
+		beginPattern = "<SerialArduino>"
+		beginIndex = leitura.index beginPattern
+		leitura = leitura[beginIndex..leitura.length]
+
+		endPattern = "</SerialArduino>"
+		endSize = endPattern.length
+		endIndex = leitura.index endPattern
+
+		@XML = leitura[0..(endIndex+endSize)]
+
+		xml = Nokogiri::XML @XML
+
+		@Amp = (xml.css "Amp").text
+		@Pot = (xml.css "Pot").text
+		@KWhdia = (xml.css "KWhdia").text
+		@KWhmes = (xml.css "KWhmes").text
+		@Conta = (xml.css "Conta").text
+
+		if params["action"]=="turnoff"
+				serialport.write "d"
+		elsif params["action"]=="turnon"
+				serialport.write "l"
+		end
+
+
 		respond_to do |format|
+			format.xml { render xml: @XML }
 			format.js
 			format.html
 		end
